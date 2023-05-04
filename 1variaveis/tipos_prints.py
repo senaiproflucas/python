d2ae1d54f3b4a686026d4ec56cf90f2abad9eedd
luizhenriquefernandes <luizhenrique.bio@gmail.com>
2023-05-04 19:49:01 -0300

    new upgrades

---
+++ 1variaveis/tipos_prints.py
@@ -0,0 +1,14 @@
+import emoji
+print('Hello World')
+print("Hello World")
+#tabela ans
+print("\033[34;3;5mHello\033[0m");
+#operações no print
+print(1+1)
+#print com aspas e números
+print('1+1')
+#print com concatenação
+print('1'+'1')
+#printando emojis deve baixar a biblioteca emoji
+print(emoji.emojize('Python é :thumbs_up:'))
+print(emoji.demojize('Python é 👍'))
---
+++ 4operadores_logicos_relacionais/operadores_logicos_relacionais.py
@@ -0,0 +1,35 @@
+'''
+Condicional if trablaha com comparações
+Se tal número é maior que outro então faça isso SENÃO faça aquilo
+como fazer isso no algorítmo
+Operadores relacionais
+maior >
+maior ou igual >=
+menor <
+menor ou igual <=
+igual ==
+diferente !=
+Operadores lógicos
+ou
+e
+não ou
+não e
+tabela verdade
+
+p   q   ou  e
+v   v   v   v
+v   f   v   f
+f   v   v   f
+f   f   f   f
+
+'''
+a = 15
+b = 14
+print(a<b)
+#if é um condicional de comparação sempre terá uma saída True ou False
+#sempre necessário trabalhar com a indentação;
+if(a>b):
+    print('a é maior que b')
+else:
+    print('a é menor que b')
+
